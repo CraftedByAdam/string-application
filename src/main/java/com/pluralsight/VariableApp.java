0f1110a205f12ae4c3dfe78217797b4c4039a6b2
Your Name <youremail@example.com>
2026-04-07 15:50:34 -0400

    I completed Step 2 and declared and initialized the correct variables. Lastly I Printed each variable into a complete working sentence.

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -5,7 +5,7 @@ import java.sql.SQLOutput;
 public class VariableApp {
     public static void main(String[] args) {
 
-        String favoriteColor = "blue";
+/*        String favoriteColor = "blue";
         System.out.println("My favourite color is: " + favoriteColor);
 
         int startYear = 2026;
@@ -18,7 +18,13 @@ public class VariableApp {
         System.out.println("Do I have a pet?: " + pet);
 
         String myMessage = "He who is not courageous enough to take risks will accomplish nothing in life. - Muhammad Ali";
-        System.out.println("My message is: " + myMessage);
+        System.out.println("My message is: " + myMessage);*/
 
+        byte daysInWeek = 7;
+        double coffeePrice = 4.99;
+        char favoriteLetter = 'A';
+        boolean raining = false;
+
+        System.out.println("There are" + " " + daysInWeek + " day in a week. " + "Did you know that in one week I only pay $" + coffeePrice + " for coffee. " + "Fun fact is that my favorite letter is  " + favoriteLetter + ". Also is it raining outside? " + raining);
+        }
     }
-}
