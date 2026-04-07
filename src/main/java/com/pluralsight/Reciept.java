d6ccdfbe403e4c6004759df32fe242bdcf6123b9
Your Name <youremail@example.com>
2026-04-07 16:33:14 -0400

    I completed Step 3 the best I could, I am stuck on getting the double (9000.00) to show up properly. Also, I created a new file called Reciept.

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+
+        System.out.println("Step 3");
+        String item = "Omega Speedmaster";
+        double price = 9000.00;
+        int item_amount_bought = 1;
+
+        System.out.println("You bought" + " " + item_amount_bought + " " + item + " " + "for" + " $" + price + ". " + "Thank you so much for you purchase.") ;
+    }
+}
