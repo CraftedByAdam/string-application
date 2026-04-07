a368db987d3d705a6ce3a0895d678746cb19450f
Your Name <youremail@example.com>
2026-04-07 15:23:00 -0400

    I completed step 1 and declared the appropriate variables and then coded the solution, finally I output my final result.

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
+
 public class VariableApp {
+    public static void main(String[] args) {
+
+        String favoriteColor = "blue";
+        System.out.println("My favourite color is: " + favoriteColor);
+
+        int startYear = 2026;
+        System.out.println("My start year of class is: " + startYear);
+
+        char middleInitial = 'A';
+        System.out.println("My middle initial is: " + middleInitial);
+
+        boolean pet = true;
+        System.out.println("Do I have a pet?: " + pet);
+
+        String myMessage = "He who is not courageous enough to take risks will accomplish nothing in life. - Muhammad Ali";
+        System.out.println("My message is: " + myMessage);
+
+    }
 }
